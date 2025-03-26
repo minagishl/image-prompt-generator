a800bbdebdd8cf0afb2c776e75f625999e866b56
minagishl <mvxd568.co@icloud.com>
2025-03-26 17:24:11 +0900

    chore: remove unnecessary options

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    base: command === "build" ? "/chroma/" : "/",
     plugins: [react(), tsconfigPaths(), tailwindcss()],
   };
 });
